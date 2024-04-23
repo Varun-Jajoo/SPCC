9b9b6d8b40f12a1d2b17de08aade3ce1909eefdb
Shreyans Jain <95974369+ShreyansJain07@users.noreply.github.com>
2024-04-23 15:34:44 +0530

    Update tac.py

--- tac.py
+++ tac.py
@@ -1,39 +1,38 @@
+def is_expression(c):
+    return c in ['+', '-', '/', '*']
+
 def main():
-    input_str = "a=b*c+d*e"
-    print(f"Tac of the string {input_str}: ")
-    input_split = input_str.split("=")
-    input_char = list(input_split[1])
-    t = [['' for _ in range(3)] for _ in range(3)]
-    count = 0
-    t_val = ''
+    exp = "a*-b+c"
+    st = []
+    mp = {}
+    n = len(exp)
+    count = 1
+    
+    for i in range(n - 1, -1, -1):
+        
+        if is_expression(exp[i]) and len(st) > 0 and is_expression(st[0]):
+            e = st.pop(0)
+            c = st.pop(0)
 
-    for i in range(len(input_char)):
-        if input_char[i] in ['/', '*', '%']:
-            t[count][0] = input_char[i - 1]
-            t[count][1] = input_char[i]
-            t[count][2] = input_char[i + 1]
+            temp = e+c
+            key = 't' + str(count)
             count += 1
-        if input_char[i] in ['+', '-']:
-            t[2][0] = '1'
-            t[2][1] = input_char[i]
-            t[2][2] = '2'
+            mp[key] = temp
+            st.insert(0, key)
+            st.insert(0, exp[i])
 
-    for i in range(3):
-        if i == 0:
-            t_val = "t1"
-        elif i == 1:
-            t_val = "t2"
+        elif not is_expression(exp[i]) and len(st) > 0 and is_expression(st[0]):
+            e = st.pop(0)
+            c = st.pop(0)
+            key = 't' + str(count)
+            count += 1
+            mp[key] = exp[i] + e + c
+            st.insert(0, key)
         else:
-            t_val = "t3"
-        print("\n" + t_val + "=", end="")
-        for j in range(3):
-            if i == 2:
-                if j == 0 or j == 2:
-                    print("t" + t[i][j], end="")
-                else:
-                    print(t[i][j], end="")
-            else:
-                print(t[i][j], end="")
-    print("\n" + input_split[0] + "=" + t_val)
+            st.insert(0, exp[i])
+    
+    for key, value in mp.items():
+        print(key, value)
 
-main()
+if __name__ == "__main__":
+    main()
