7e3d712274853672a2d98afd8e64fdbee805c035
Shreyans Jain <95974369+ShreyansJain07@users.noreply.github.com>
2024-04-24 01:55:53 +0530

    Update ll1.py

--- ll1.py
+++ ll1.py
@@ -1,91 +1,61 @@
+data = {
+    'E': ['TX'],
+    'X': ['+TX', 'e'],
+    'T': ['FY'],
+    'Y': ['*FY', 'e'],
+    'F': ['id', '(E)']
 
-def find(arr , value):
-    for i in arr:
-        if i == value:
-            return True
-    return False
-
-
-data = [ ['E', ['TA']], ['A', ['aTA', 'è']], ['T', ['FB']], ['B', ['bFB', 'è']], ['F', ['c', 'dEf']] ]
-
+}
 first = {
-    "TA" : ['d', 'c'],
-    "aTA" : ['a'],
-    'FB': ['c', 'd'],
-    'bFB' : ['b'],
-    'c' : ['c'],
-    'dEf' : ['d']
+    'F': {'id': ['id'], '(E)': ['(']},
+    'Y': {'*FY': ['*'], 'e': ['e']},
+    'T': {'FY': ['id', '(']},
+    'X': {'+TX': ['+'], 'e': ['e']},
+    'E': {'TX': ['id', '(']},
 }
 
 follow = {
-    "E" : ['$'],
-    'A' : ['$', 'f'],
-    'T' : ['a', '$', 'f'],
-    'B' : ['a', '$', 'f'],
-    'F' : ['b', 'a' , '$', 'f']
-
+    "E": [')', '$'],
+    'X': [')', '$'],
+    'T': ['+', ')', '$'],
+    'Y': ['+', ')', '$'],
+    'F': ['+', '*', ')', '$']
 }
 
-terminals = []
-nonT = []
 
-for i in range(len(data)):
-    nonT.append(data[i][0])
-    for j in data[i][1]:
-        for k in j:
-            #print(k)
-            if k.islower() and k != 'è' and not find(terminals, k):
-                terminals.append(k)
+def ll1(data, first, follow):
+    terminal = []
+    l = {}
+    for i in first:
+        for j in first[i]:
+            for k in first[i][j]:
+                if k != 'e':
+                    l[i, k] = j
+                    terminal.append(k)
+                else:
+                    for a in follow[i]:
+                        l[i, a] = j
+    for i in follow:
+        for j in follow[i]:
+            if j != '$':
+                terminal.append(j)
+    terminal = list(set(terminal))
+    key = l.keys()
+    terminal = sorted(terminal)
+    terminal.append('$')
+    
+    for i in terminal:
+        print("\t\t\t  " + i, end='')
+    print("\n", "=" * 80)
+    for i in data:
+        print(i + "\t\t", end='')
+        for j in terminal:
+            if ((i, j) in key):
+                print(f"| {i}--->{l[i, j]}\t", end='')
+            else:
+                print("| \t\t\t", end='')
 
-terminals.append("$")
-
-print("Terminals : ", " ".join(terminals))
-print("\nFirst :")
-for k, v in first.items():
-    print(k, " -> " ," ,".join(v))
-
-print("\nFollow :")
-
-for k, v in follow.items():
-    print(k, " -> " , " ,".join(v))
-
-table = {}
-
-for i in range(len(nonT)):
-    table[nonT[i]] = {}
-    for j in terminals:
-        table[nonT[i]][j] = []
-
-
-for i in range(len(data)):
-    left = data[i][0]
-    #print(left)
-    right = data[i][1]
-    #print(right)
-
-    for j in right:
-        #print(j)
-        if j == "è":
-            for itr in follow[left]:
-                #print(itr)
-                table[left][itr].append(f'{left}->{j}')
-        else:
-            for itr in first[j]:
-                #print(itr)
-                table[left][itr].append(f'{left}->{j}')
-                #print(table)
+        print("\n", "=" * 80)
 
 
-print("\nParsing Table :")
-for i in terminals:
-    print(f' \t{i}', end="")
-print()
-
-for non_term in nonT:
-    print(non_term, end="\t")
-    for term in terminals:
-        productions = table[non_term][term]
-        if productions:
-            print(" ".join(productions), end=" ")
-        print("\t", end="")
-    print()
+ll1(data, first, follow)
