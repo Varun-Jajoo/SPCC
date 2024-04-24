bfbcb9a04583b704f6f151cda149351431e14dbd
Shreyans Jain <95974369+ShreyansJain07@users.noreply.github.com>
2024-04-24 10:52:11 +0530

    Update follow.py

--- follow.py
+++ follow.py
@@ -1,68 +1,76 @@
-data = [
-    ['S', ['aBDh']],
-    ['B', ['cC']],
-    ['C', ['bC', 'è']],
-    ['D', ['EF']],
-    ['E', ['g', 'è']],
-    ['F', ['f', 'è']]
-]
-
-first = {
-    'S': {'a'},
-    'B': {'c'},
-    'C': {'b', 'è'},
-    'D': {'g', 'f', 'è'},
-    'E': {'g', 'è'},
-    'F': {'f', 'è'}
+data = {
+   "S":['aBDh'],
+   'B':['cC'],
+   'C':['bC','e'],
+   'D':['EF'],
+   'E':['g','e'],
+   'F':['f','e']
 }
 
+first = {
+   "S":['a'],
+   'B':['c'],
+   'C':['b','e'],
+   'D':['g','f','e'],
+   'E':['g','e'],
+   'F':['f','e']
+}
+opp={}
 follow = {}
 
-for i in range(len(data)):
-    left = data[i][0]
-    temp_follow = set()
+# data = {
+#     'S': ['TE'],
+#     'E': ['+TE', 'e'],
+#     'T': ['FY'],
+#     'Y': ['*FY', 'e'],
+#     'F': ['(S)', 'id']
+# }
+# first = {
+#     'F': ['(', 'i'],
+#     'Y': ['*', 'e'],
+#     'T': ['(', 'i'],
+#     'E': ['+', 'e'],
+#     'S': ['(', 'i']
+# }
+# opp = ['(', ')', '+', '*', '-']
 
-    if i == 0:
-        temp_follow.add("$")
 
-    for j in range(len(data)):
-        for k in data[j][1]:
-            index = k.find(left)
-
-            if index == -1:
+def fol(i, follow):
+    temp = []
+    if i == "S":
+        temp.append("$")
+    for j in data:
+        for k in data[j]:
+            Index = k.find(i)
+            if Index == -1:
                 continue
-
-            # NT at end
-            if index == len(k) - 1:
-                if left != data[j][0]:
-                    for ele in follow[data[j][0]]:
-                        temp_follow.add(ele)
-
-            # T after NT in follow
-            elif k[index + 1].islower():
-                temp_follow.add(k[index + 1])
-
-            # NT after NT in follow
+            p = Index + 1
+            if p != len(k):
+                print(k[p])
+                if k[p].islower() or k[p] in opp:
+                    temp.append(k[p])
+                else:
+                    for u in first[k[p]]:
+                        if 'e' != u:
+                            print("uuuu", u)
+                            temp.append(u)
+                    for u in fol(k[p], follow):
+                        temp.append(u)
             else:
-                next_index = index + 1
-                while next_index < len(k):
-                    next_symbol = k[next_index]
-                    if next_symbol.islower():
-                        temp_follow.add(next_symbol)
-                        break
-                    else:
-                        for ele in first[next_symbol]:
-                            if ele != 'è':
-                                temp_follow.add(ele)
-                        if 'è' not in first[next_symbol]:
-                            break
-                        next_index += 1
-                if next_index == len(k):
-                    if left != data[j][0]:
-                        for ele in follow[data[j][0]]:
-                            temp_follow.add(ele)
+                if i != j:
+                    for o in follow[j]:
+                        if o != 'e':
+                            print("hello    ", o)
+                            temp.append(o)
 
-    follow[left] = temp_follow
+    return temp
+
+
+for i in data:
+    temp = fol(i, follow)
+
+    follow[i] = list(set(temp))
+    print("temp", follow)
 
 print("Follow : ")
 print(follow)
